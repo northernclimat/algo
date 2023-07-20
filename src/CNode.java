2c214cf23accca630f9d80b8ac305c594447095e
Denis Kozin(home) <70297257+Akishev23@users.noreply.github.com>
2023-07-21 02:01:46 +0300

    made 3

---
+++ src/CNode.java
@@ -0,0 +1,7 @@
+public class CNode {
+    int data;
+    CNode parent;
+    CNode left;
+    CNode right;
+    int color; // 1 . Red, 0 . Black
+}
---
+++ src/RedBlackTree.java
@@ -0,0 +1,146 @@
+public class RedBlackTree {
+    private CNode root;
+    private CNode TNULL;
+
+    // Preorder
+    private void preOrderHelper(CNode node) {
+        if (node != TNULL) {
+            System.out.print(node.data + " ");
+            preOrderHelper(node.left);
+            preOrderHelper(node.right);
+        }
+    }
+
+    // Rotate left
+    private void leftRotate(CNode x) {
+        CNode y = x.right;
+        x.right = y.left;
+        if (y.left != TNULL) {
+            y.left.parent = x;
+        }
+        y.parent = x.parent;
+        if (x.parent == null) {
+            this.root = y;
+        } else if (x == x.parent.left) {
+            x.parent.left = y;
+        } else {
+            x.parent.right = y;
+        }
+        y.left = x;
+        x.parent = y;
+    }
+
+    // Rotate right
+    private void rightRotate(CNode x) {
+        CNode y = x.left;
+        x.left = y.right;
+        if (y.right != TNULL) {
+            y.right.parent = x;
+        }
+        y.parent = x.parent;
+        if (x.parent == null) {
+            this.root = y;
+        } else if (x == x.parent.right) {
+            x.parent.right = y;
+        } else {
+            x.parent.left = y;
+        }
+        y.right = x;
+        x.parent = y;
+    }
+
+    // Insert node
+    public void insert(int key) {
+        CNode node = new CNode();
+        node.parent = null;
+        node.data = key;
+        node.left = TNULL;
+        node.right = TNULL;
+        node.color = 1; // new node is red
+
+        CNode y = null;
+        CNode x = this.root;
+
+        while (x != TNULL) {
+            y = x;
+            if (node.data < x.data) {
+                x = x.left;
+            } else {
+                x = x.right;
+            }
+        }
+
+        node.parent = y;
+        if (y == null) {
+            root = node;
+        } else if (node.data < y.data) {
+            y.left = node;
+        } else {
+            y.right = node;
+        }
+
+        if (node.parent == null){
+            node.color = 0;
+            return;
+        }
+
+        if (node.parent.parent == null) {
+            return;
+        }
+
+        fixInsert(node);
+    }
+
+    // Fix the tree
+    private void fixInsert(CNode k){
+        CNode u;
+        while (k.parent.color == 1) {
+            if (k.parent == k.parent.parent.right) {
+                u = k.parent.parent.left; // uncle
+                if (u.color == 1) {
+                    u.color = 0;
+                    k.parent.color = 0;
+                    k.parent.parent.color = 1;
+                    k = k.parent.parent;
+                } else {
+                    if (k == k.parent.left) {
+                        k = k.parent;
+                        rightRotate(k);
+                    }
+                    k.parent.color = 0;
+                    k.parent.parent.color = 1;
+                    leftRotate(k.parent.parent);
+                }
+            } else {
+                u = k.parent.parent.right; // uncle
+
+                if (u.color == 1) {
+                    u.color = 0;
+                    k.parent.color = 0;
+                    k.parent.parent.color = 1;
+                    k = k.parent.parent;
+                } else {
+                    if (k == k.parent.right) {
+                        k = k.parent;
+                        leftRotate(k);
+                    }
+                    k.parent.color = 0;
+                    k.parent.parent.color = 1;
+                    rightRotate(k.parent.parent);
+                }
+            }
+            if (k == root) {
+                break;
+            }
+        }
+        root.color = 0;
+    }
+
+    public RedBlackTree() {
+        TNULL = new CNode();
+        TNULL.color = 0;
+        TNULL.left = null;
+        TNULL.right = null;
+        root = TNULL;
+    }
+}
